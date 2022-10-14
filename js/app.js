d10d24cf92abc320021db58572f7884541fc3b08
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2022-10-14 15:31:29 +0200

    palindroma

---
+++ js/app.js
@@ -0,0 +1,26 @@
+const userInput = document.querySelector('input');
+
+const btn = document.querySelector('button');
+
+const output = document.querySelector('h1');
+
+
+function inputCheck() {
+
+    let currentInput = userInput.value.toLowerCase();
+
+    let reversedInput = currentInput.split('').reverse().join('');
+
+    if (currentInput == reversedInput) {
+
+        output.innerText = 'Parola palindroma';
+
+    } else {
+        output.innerText = 'Parola non palindroma';
+    }
+
+    console.log(currentInput, reversedInput);
+
+}
+
+btn.addEventListener('click', inputCheck);
